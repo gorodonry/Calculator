db75253a0ce6ac34a4ded89539562dbd5767107b
Ryan Gordon <gorodonry@proton.me>
2025-01-19 15:46:55 +1300

    Add javadocs to Query class

--- src/Query.java
+++ src/Query.java
@@ -1,1 +1,7 @@
+/**
+ * Represents a query made by the user.
+ *
+ * @param expression the mathematical expression entered by the user.
+ * @param answer the answer to the expression.
+ */
 public record Query(String expression, double answer) {}
