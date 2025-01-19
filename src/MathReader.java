5f511b932e81b164c37f950295e1368d53e9cf78
Ryan Gordon <gorodonry@proton.me>
2025-01-19 20:48:21 +1300

    Refactor checking of brackets and tidy up javadocs

--- src/MathReader.java
+++ src/MathReader.java
@@ -3,29 +3,29 @@ import java.util.*;
 /**
  * Contains various methods for interpreting an input string as a mathematical expression.
  */
-public abstract class MathReader {
-    public static String[] BRACKETS = { "(", ")", "[", "]" };
+public final class MathReader {
+    public static final Map<Character, Character> brackets = Map.ofEntries(
+            Map.entry('(', ')'),
+            Map.entry('[', ']')
+    );
 
     /**
-     * Determines whether an expression contains empty brackets i.e. contains "( )".
+     * Determines whether an expression contains empty brackets i.e. contains "()" or "[]".
+     *
      * @param input the expression to check for empty brackets.
+     *
      * @return true if the expression contains empty brackets.
      */
     public static boolean containsEmptyBrackets(String input) {
-        Queue<Character> remainingChars = new ArrayDeque<>(List.of(input.chars()
-                .mapToObj(c -> (char) c)
-                .toArray(Character[]::new)));
-        while (!remainingChars.isEmpty()) {
-            if (remainingChars.peek() == '(' || remainingChars.peek() == '[') {
-                remainingChars.poll();
-                if (!remainingChars.isEmpty()) {
-                    if (remainingChars.peek() == ')' || remainingChars.peek() == ']') {
-                        return true;
-                    }
-                }
-            }
+        Queue<Character> remainingChars = new ArrayDeque<>(
+                input.chars().mapToObj(c -> (char) c).toList()
+        );
 
-            remainingChars.poll();
+        while (remainingChars.size() > 1) {
+            if (brackets.containsKey(remainingChars.poll()) &&
+                    brackets.containsValue(Objects.requireNonNull(remainingChars.peek()))) {
+                return true;
+            }
         }
 
         return false;
@@ -33,53 +33,50 @@ public abstract class MathReader {
 
     /**
      * Checks that all the brackets in a string are matching. Note: only checks for '(..)' and '[..]'.
+     *
      * @param input the string to check.
+     *
      * @return true if all brackets are matching.
      */
     public static boolean bracketsMatching(String input) {
-        Stack<Character> brackets = new Stack<>();
-        Queue<Character> remainingChars = new ArrayDeque<>(List.of(input.chars()
-                .mapToObj(c -> (char) c)
-                .toArray(Character[]::new)));
+        Stack<Character> unmatchedBrackets = new Stack<>();
+        Queue<Character> remainingChars = new ArrayDeque<>(
+                input.chars().mapToObj(c -> (char) c).toList()
+        );
+
         while (!remainingChars.isEmpty()) {
-            switch (remainingChars.poll()) {
-                case '(' -> brackets.push('(');
-                case '[' -> brackets.push('[');
-                case ')' -> {
-                    if (brackets.isEmpty())
-                        return false;
+            var character = remainingChars.poll();
 
-                    if (brackets.pop() != '(')
-                        return false;
-                }
-                case ']' -> {
-                    if (brackets.isEmpty())
-                        return false;
+            if (brackets.containsKey(character)) {
+                unmatchedBrackets.push(character);
+            }
 
-                    if (brackets.pop() != '[')
-                        return false;
-                }
-                default -> {}
+            if (brackets.containsValue(character) && brackets.get(unmatchedBrackets.pop()) != character) {
+                return false;
             }
         }
 
-        return brackets.isEmpty();
+        return unmatchedBrackets.isEmpty();
     }
 
     /**
      * Interprets a string as a mathematical expression by grouping components together.
+     *
      * @param input the string to interpret as a mathematical expression as a list e.g.
-     *  -> ["s", "i", "n", "(", "5.32", "+", "67", ")", "/", "l", "o", "g", "-2"].
+     *              ["s", "i", "n", "(", "5.32", "+", "67", ")", "/", "l", "o", "g", "-2"].
      * @param errorLog a list for recording any errors encountered while attempting to interpret the string.
      * @param constants a map of all named constants. Note user defined constants are upper case letters and are named,
-     *  but may be keyed to empty optionals.
+     *                  but may be keyed to empty optionals.
+     *
      * @return a list of components representing the expression that was interpreted, or an empty optional if errors
-     *  were encountered. From the above example, this method would return
-     *  -> ["sin", "(", "5.32", "+", "67", ")", "/", "log", "-2"]
+     *         were encountered. From the above example, this method would return
+     *         ["sin", "(", "5.32", "+", "67", ")", "/", "log", "-2"]
      */
-    public static Optional<List<String>> readExpression(List<String> input,
-                                                        List<String> errorLog,
-                                                        Map<String, Optional<Double>> constants) {
+    public static Optional<List<String>> readExpression(
+            List<String> input,
+            List<String> errorLog,
+            Map<String, Optional<Double>> constants
+    ) {
         List<String> currentWorkingSection = new ArrayList<>();
         List<String> formattedExpression = new ArrayList<>();
 
@@ -133,6 +130,7 @@ public abstract class MathReader {
 
     /**
      * Appends a number to the expression, taking into account the possibility of a minus sign preceding the number.
+     *
      * @param partiallyFormattedExpression the formatted expression so far, to append the number to.
      * @param number the number to append.
      */
@@ -143,7 +141,7 @@ public abstract class MathReader {
         // occurs; i.e. when a minus is surrounded by bracket(s) and/or number(s).
         if (numberOfSections >= 2) {
             if (partiallyFormattedExpression.get(numberOfSections - 1).equals("-") &&
-                    CMath.OPERATION_ORDERS.containsKey(partiallyFormattedExpression.get(numberOfSections - 2))) {
+                    CMath.operations.containsKey(partiallyFormattedExpression.get(numberOfSections - 2))) {
                 number *= -1;
                 partiallyFormattedExpression.remove(numberOfSections - 1);
             }
@@ -169,40 +167,39 @@ public abstract class MathReader {
     /**
      * Appends a command/series of commands to the expression. Also recognises pi and e as 'commands', and substitutes
      *  them for the numbers they represent.
+     *
      * @param partiallyFormattedExpression the formatted expression so far, to append the command(s) to.
      * @param input the command(s) to append.
      * @param constants a map of all named constants. Note user defined constants are upper case letters and are named,
-     *  but may be keyed to empty optionals.
+     *                  but may be keyed to empty optionals.
      * @param errorLog a list for recording any errors encountered while attempting to interpret the command(s).
      */
-    private static void appendCommand(List<String> partiallyFormattedExpression,
-                                      List<String> input,
-                                      List<String> errorLog,
-                                      Map<String, Optional<Double>> constants) {
+    private static void appendCommand(
+            List<String> partiallyFormattedExpression,
+            List<String> input,
+            List<String> errorLog,
+            Map<String, Optional<Double>> constants
+    ) {
         List<String> command = new ArrayList<>();
 
         for (String component : input) {
             // Check for single letter constants. Note we have to check the command is currently empty because of e,
             // which can also be found in function names such as secant. User defined constants are upper case letters.
             if (constants.containsKey(component) && command.isEmpty()) {
-                if (constants.get(component).isPresent()) {
-                    appendNumber(partiallyFormattedExpression, Math.E);
-
-                    continue;
-                } else {
-                    errorLog.add(String.format("Undefined constant: %s", component));
-                }
+                constants.get(component).ifPresentOrElse(
+                        value -> appendNumber(partiallyFormattedExpression, value),
+                        () -> errorLog.add(String.format("Undefined constant: %s", component))
+                );
             }
 
             // Check for brackets.
-            if (Arrays.asList(BRACKETS).contains(component)) {
+            if (brackets.contains(component)) {
                 if (!command.isEmpty()) {
                     errorLog.add(String.format("Unrecognised command: %s", String.join("", command)));
                     command.clear();
                 }
 
                 partiallyFormattedExpression.add(component);
-
                 continue;
             }
 
@@ -218,11 +215,10 @@ public abstract class MathReader {
                 }
 
                 command.clear();
-
                 continue;
             }
 
-            if (CMath.OPERATION_ORDERS.containsKey(String.join("", command))) {
+            if (CMath.operations.containsKey(String.join("", command))) {
                 partiallyFormattedExpression.add(String.join("", command));
                 command.clear();
             }
